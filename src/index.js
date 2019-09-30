808ec8d70e8c51baf474476d8785c92def8f6adf
Anzhela Martsinovich <anzhela.martsinovich@gmail.com>
2019-09-30 19:53:20 +0300

    refactor: short variant

--- src/index.js
+++ src/index.js
@@ -9,45 +9,12 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
+     
+    if(knowsProgramming) {
       // если у студента есть базовые знания в программировании
-      if (knowsProgramming === true && focus === 'family') {
-        return Math.ceil(800 / config.family);
-      }
-      if (knowsProgramming === true && focus === 'friends') {
-        return Math.ceil(800 / config.friends);
-      }
-      if (knowsProgramming === true && focus === 'normal_life') {
-        return Math.ceil(800 / config.normal_life);
-      }
-      if (knowsProgramming === true && focus === 'profession') {
-        return Math.ceil(800 / config.profession);
-      }
-      if (knowsProgramming === true && focus === 'carrier') {
-        return Math.ceil(800 / config.carrier);
-      }
-      if (knowsProgramming === true && focus === 'top_peformance') {
-        return Math.ceil(800 / config.top_peformance);
-      }
-
-      // студент без знаний в программировании
-      if (knowsProgramming === false && focus === 'family') {
-        return Math.ceil(1300 / config.family);
-      }
-      if (knowsProgramming === false && focus === 'friends') {
-        return Math.ceil(1300 / config.friends);
-      }
-      if (knowsProgramming === false && focus === 'normal_life') {
-        return Math.ceil(1300 / config.normal_life);
-      }
-      if (knowsProgramming === false && focus === 'profession') {
-        return Math.ceil(1300 / config.profession);
-      }
-      if (knowsProgramming === false && focus === 'carrier') {
-        return Math.ceil(1300 / config.carrier);
-      }
-      if (knowsProgramming === false && focus === 'top_peformance') {
-        return Math.ceil(1300 / config.top_peformance);
-      }
-
+      return Math.ceil(800 / config[focus]);
+    }
+    // студент без знаний в программировании
+    return Math.ceil(1300 / config[focus]);
   };
   
